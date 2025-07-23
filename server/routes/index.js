994a69ad96f6523d2ac5dd4bfa7e952f769012bc
Arpit Bhalla <64216681+arpitbhalla1801@users.noreply.github.com>
2025-07-23 12:35:19 +0530

    feat: add features page with detailed descriptions and styling; update routing and header links

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ const mainController = require('../controllers/mainController');
  * App Routes
  */
 router.get('/' , mainController.homepage);
+router.get('/features' , mainController.features);
 router.get('/about' , mainController.about);
 router.get('/faq' , mainController.faq);
 
